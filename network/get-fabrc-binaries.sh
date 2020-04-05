bc4eba4a13f77cf8176d52256f3366487584d2ce
Ubuntu <ubuntu@ip-172-31-18-197.us-east-2.compute.internal>
2020-04-05 12:56:27 +0000

    High volume of changes are done in scripts and yaml files

---
+++ network/get-fabrc-binaries.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -eu
+# Copyright London Stock Exchange Group All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# This script get all fabric binaries from github repo
+
+set -ev
+
+ENV_LOCATION=$PWD/.env
+echo $ENV_LOCATION
+source $ENV_LOCATION
+
+rm $PWD/bin/*	# remove old binaries
+
+BINARY_FILE=hyperledger-fabric-${OS_ARCH}-${FABRIC_VERSION}.tar.gz
+CA_BINARY_FILE=hyperledger-fabric-ca-${OS_ARCH}-${FABRIC_CA_VERSION}.tar.gz
+
+pullBinaries() {
+	echo "===> Downloading version ${FABRIC_VERSION} platform specific fabric binaries"
+	set -x
+	download "${BINARY_FILE}" "https://github.com/hyperledger/fabric/releases/download/v${FABRIC_VERSION}/${BINARY_FILE}"
+	set +x
+	if [ $? -eq 22 ]; then
+		echo
+		echo "------> ${FABRIC_VERSION} platform specific fabric binary is not available to download <----"
+		echo
+		exit
+	fi
+
+	echo "===> Downloading version ${FABRIC_CA_VERSION} platform specific fabric-ca-client binary"
+	set -x
+	download "${CA_BINARY_FILE}" "https://github.com/hyperledger/fabric-ca/releases/download/v${FABRIC_CA_VERSION}/${CA_BINARY_FILE}"
+	set +x
+	if [ $? -eq 22 ]; then
+		echo
+		echo "------> ${FABRIC_CA_VERSION} fabric-ca-client binary is not available to download  (Available from 1.1.0-rc1) <----"
+		echo
+		exit
+	fi
+}
+
+# This will download the .tar.gz
+download() {
+    local BINARY_FILE=$1
+    local URL=$2
+    echo "===> Downloading: " "${URL}"
+    set -x
+    curl -L --retry 5 --retry-delay 3 "${URL}" | tar xz || rc=$?
+    set +x
+    if [ -n "$rc" ]; then
+        echo "==> There was an error downloading the binary file."
+        return 22
+    else
+        echo "==> Done."
+    fi
+}
+
+cd $PWD/bin
+
+pullBinaries
+
+cd -
+
