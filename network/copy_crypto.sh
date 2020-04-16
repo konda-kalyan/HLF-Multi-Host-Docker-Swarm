9b69a75a2d0b239856b7f015dc1f33121ba870fe
Ubuntu <ubuntu@ip-172-31-13-137.us-east-2.compute.internal>
2020-04-16 21:24:32 +0000

    Orderer docker compose configs are changed

--- network/copy_crypto.sh
+++ network/copy_crypto.sh
@@ -6,11 +6,11 @@ rm -rf mkdir /var/mynetwork/*
 mkdir -p /var/mynetwork/chaincode
 mkdir -p /var/mynetwork/certs
 mkdir -p /var/mynetwork/bin
-mkdir -p /var/mynetwork/fabric-src
+#mkdir -p /var/mynetwork/fabric-src
 
-git clone https://github.com/hyperledger/fabric /var/mynetwork/fabric-src/hyperledger/fabric
-cd /var/mynetwork/fabric-src/hyperledger/fabric
-git checkout release-1.4
+#git clone https://github.com/hyperledger/fabric /var/mynetwork/fabric-src/hyperledger/fabric
+#cd /var/mynetwork/fabric-src/hyperledger/fabric
+#git checkout release-1.4
 cd -
 cp -R crypto-config /var/mynetwork/certs/
 cp -R config /var/mynetwork/certs/
