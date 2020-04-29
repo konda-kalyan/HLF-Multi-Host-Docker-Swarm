868b4468c3de46e2ae37767fabde3380a30c1a70
Ubuntu <ubuntu@ip-172-31-8-85.us-east-2.compute.internal>
2020-04-29 10:27:27 +0000

    Tested with sample java chaincode chaincode_example02 from fabric_samples. Running fine.

--- network/STEP5_create_channel_then_do_chaincode_operations.sh
+++ network/STEP5_create_channel_then_do_chaincode_operations.sh
@@ -1,2 +1,3 @@
 ./scripts/create_join_channel.sh
-./scripts/install_instantiate_invoke_chaincode.sh
+#./scripts/install_instantiate_invoke_chaincode.sh
+./scripts/install_instantiate_invoke_java_chaincode.sh
--- network/bring_down_whole_network_on_all_machines.sh
+++ network/bring_down_whole_network_on_all_machines.sh
@@ -12,10 +12,12 @@ sleep 10
 
 # Clean things on ORG1 first
 docker swarm leave -f
-docker network rm docker_gwbridge
+#docker network rm docker_gwbridge
+docker system prune -f
+docker rmi $(docker images | grep "peer0.org1.example.com" | awk '{print $3}')
 
 # Clean things on ORG2 next
-ssh ubuntu@$WORKER_NODE1_HOSTNAME 'docker swarm leave -f; docker network rm docker_gwbridge'
+ssh ubuntu@$WORKER_NODE1_HOSTNAME 'docker swarm leave -f; docker system prune -f'
 
 # Clean things on ORG2 next
-ssh ubuntu@$WORKER_NODE2_HOSTNAME 'docker swarm leave -f; docker network rm docker_gwbridge'
+ssh ubuntu@$WORKER_NODE2_HOSTNAME 'docker swarm leave -f; docker system prune -f'
