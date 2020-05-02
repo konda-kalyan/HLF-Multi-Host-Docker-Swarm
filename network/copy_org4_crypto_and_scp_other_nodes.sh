23c8ab34a2fcdf15803335c5c596466bd97db8e9
Ubuntu <ubuntu@ip-172-31-8-85.us-east-2.compute.internal>
2020-05-02 04:41:23 +0000

    Org4 docker-compose files have been added

---
+++ network/copy_org4_crypto_and_scp_other_nodes.sh
@@ -0,0 +1,9 @@
+
+sudo cp -R ./org4-artifacts/crypto-config/peerOrganizations/* /var/mynetwork/certs/crypto-config/peerOrganizations
+
+#copy artifacts to Org4 nodes
+scp -r channel-artifacts crypto-config org4-artifacts ubuntu@ip-172-31-37-174:~/HLF-Multi-Host-Docker-Swarm/network
+
+#On Org4, copy artifacts to command dir (/var/mynetwork/)
+ssh ubuntu@ip-172-31-37-174 'cd ~/HLF-Multi-Host-Docker-Swarm/network; ./copy_crypto.sh; sudo cp -R ./org4-artifacts/crypto-config/peerOrganizations/* /var/mynetwork/certs/crypto-config/peerOrganizations;'
+
