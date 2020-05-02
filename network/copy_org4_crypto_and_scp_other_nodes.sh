ef11a2ff83619469d6b2d71e032127c5b49e486b
Ubuntu <ubuntu@ip-172-31-8-85.us-east-2.compute.internal>
2020-05-02 13:13:44 +0000

    org4 script change

--- network/copy_org4_crypto_and_scp_other_nodes.sh
+++ network/copy_org4_crypto_and_scp_other_nodes.sh
@@ -1,9 +1,13 @@
+ENV_LOCATION=$PWD/.env
+echo $ENV_LOCATION
+source $ENV_LOCATION
 
 sudo cp -R ./org4-artifacts/crypto-config/peerOrganizations/* /var/mynetwork/certs/crypto-config/peerOrganizations
 
 #copy artifacts to Org4 nodes
 scp -r channel-artifacts crypto-config org4-artifacts ubuntu@ip-172-31-37-174:~/HLF-Multi-Host-Docker-Swarm/network
-scp -r ../chaincodes/* ubuntu@ip-172-31-37-174:~/HLF-Multi-Host-Docker-Swarm/chaincodes
+scp -r ../chaincodes/ ubuntu@ip-172-31-37-174:~/HLF-Multi-Host-Docker-Swarm
+scp -r ../chaincodes/ ubuntu@ip-172-31-37-174:/var/mynetwork/
 
 #On Org4, copy artifacts to command dir (/var/mynetwork/)
 ssh ubuntu@ip-172-31-37-174 'cd ~/HLF-Multi-Host-Docker-Swarm/network; ./copy_crypto.sh; sudo cp -R ./org4-artifacts/crypto-config/peerOrganizations/* /var/mynetwork/certs/crypto-config/peerOrganizations;'
--- network/scp_artifacts_to_worker_nodes_and_copy_to_common_dir.sh
+++ network/scp_artifacts_to_worker_nodes_and_copy_to_common_dir.sh
@@ -15,6 +15,10 @@ source $ENV_LOCATION
 scp -r channel-artifacts crypto-config ubuntu@$WORKER_NODE1_HOSTNAME:~/HLF-Multi-Host-Docker-Swarm/network
 scp -r channel-artifacts crypto-config ubuntu@$WORKER_NODE2_HOSTNAME:~/HLF-Multi-Host-Docker-Swarm/network
 
+# copy chaincodes as well
+scp -r ../chaincodes/ ubuntu@$WORKER_NODE1_HOSTNAME:~/HLF-Multi-Host-Docker-Swarm/
+scp -r ../chaincodes/ ubuntu@$WORKER_NODE2_HOSTNAME:~/HLF-Multi-Host-Docker-Swarm/
+
 #On Workder nodes, copy artifacts to command dir (/var/mynetwork/)
 ssh ubuntu@$WORKER_NODE1_HOSTNAME 'cd ~/HLF-Multi-Host-Docker-Swarm/network; ./copy_crypto.sh'
 ssh ubuntu@$WORKER_NODE2_HOSTNAME 'cd ~/HLF-Multi-Host-Docker-Swarm/network; ./copy_crypto.sh'
